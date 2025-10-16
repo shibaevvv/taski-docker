966ab778af132090600033b01b664b323f03aa64
Vladimir Shibaev <vladimir.shibaev@gmail.com>
2025-10-16 11:12:19 +0300

    Fix name ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
